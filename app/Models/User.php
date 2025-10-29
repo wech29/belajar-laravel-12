7d36960ab67d5bd243dcbcc01be7c577b0c8955d
fariez11 <fariezilham03@gmail.com>
2025-10-29 16:44:44 +0700

    mini update

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -43,7 +42,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
         ];
     }
 
--- app/View/Components/navbar.php
+++ app/View/Components/navbar.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace App\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class navbar extends Component
+class Navbar extends Component
 {
     /**
      * Create a new component instance.
@@ -19,7 +18,7 @@ class navbar extends Component
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View | Closure | string
     {
         return view('components.navbar');
     }
